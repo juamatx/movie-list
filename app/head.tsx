b5bc3a216180952ea6a0e0cc2f8d6c08d23812e4
juamatx <60664640+juamatx@users.noreply.github.com>
2022-12-09 14:19:49 -0500

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Popcorn Time</title>
+      <title>Moovi Time</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="New recommendation every week" />
       <link rel="icon" href="/favicon.ico" />
