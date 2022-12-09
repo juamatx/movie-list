40a1b1fe8a00a598cf90428352fef8e316d085ee
juamatx <60664640+juamatx@users.noreply.github.com>
2022-12-09 13:46:04 -0500

    Update select_movie.tsx

--- app/select_movie.tsx
+++ app/select_movie.tsx
@@ -1,19 +1,24 @@
 import styles from "./page.module.css";
 import data from "./movies.json";
 
-function randomMovie(movies:any){
-  const moviesArray = Object.keys(movies).map((key) => movies[key]);
-  const randomMovie = moviesArray[Math.floor(Math.random() * moviesArray.length)];
-  return randomMovie;
+async function getMovie() {
+  const res = await fetch(
+    "https://y23xzzq934.execute-api.us-east-1.amazonaws.com/Prod/movie-of-the-week"
+  );
 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-export default function Movie() {
-  console.log(data);
-  const movie = randomMovie(data);
+export default async function Movie() {
+  const movie = await getMovie();
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{movie.title}</h1>
+      <h1 className={styles.title}>{movie}</h1>
     </div>
   );
 }
