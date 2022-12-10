090537b700d11a1118598acb15f2b08d14f0e907
juamatx <60664640+juamatx@users.noreply.github.com>
2022-12-10 14:05:00 -0500

    added revalidate line

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+export const revalidate = 0;
 import styles from "./page.module.css";
 
 async function getMovie() {
-  const res = await fetch( "https://y23xzzq934.execute-api.us-east-1.amazonaws.com/Prod/movie-of-the-week");
+  const res = await fetch(
+    "https://y23xzzq934.execute-api.us-east-1.amazonaws.com/Prod/movie-of-the-week"
+  );
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
@@ -13,7 +16,7 @@ export default async function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-       <h1 className={styles.title}>{movie}</h1>
+        <h1 className={styles.title}>{movie}</h1>
       </main>
     </div>
   );
